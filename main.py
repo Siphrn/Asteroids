5643f48a8cb16cf5fe73fcecdae64607b3d5c53e
siphrn <lucaskordzakhia@yahoo.com>
2026-05-01 15:29:46 -0400

    Added two new groups, `updatable` and `drawable`, to manage the update and draw calls for game objects. The `Player` class is added to both groups, allowing for cleaner code in the main game loop. The update and draw calls are now made on the groups instead of individual objects, improving scalability as more game objects are added in the future.

--- main.py
+++ main.py
@@ -14,13 +14,21 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
-        player.update(dt)
+        updatable.update(dt)
         screen.fill("black")
-        player.draw(screen)
+        for thing in drawable:
+            thing.draw(screen)
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
