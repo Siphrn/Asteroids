6de140110534b4b18367215046c89a1462e85b1a
siphrn <lucaskordzakhia@yahoo.com>
2026-05-01 15:50:58 -0400

    Added asteroids, and an asteroid field. Collisions TBA

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from circleshape import CircleShape
 from player import Player
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     # Log some initial info about the game environment
@@ -26,10 +28,14 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
     # Game Loop
 
