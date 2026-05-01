cb6ac2c84c16a59b822d5861df42217f28546fd4
siphrn <lucaskordzakhia@yahoo.com>
2026-04-30 23:00:03 -0400

    Created player class and added player drawing to main loop.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+from circleshape import CircleShape
+from player import Player
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 
@@ -12,9 +14,12 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         log_state()
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
